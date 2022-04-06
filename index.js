37d28f60ed85480f53eeea68d243e4f416f72dd1
Kevin Horwitz <khorwitz1992@gmail.com>
2022-04-05 21:42:35 -0500

    if statements for gamePlay

--- index.js
+++ index.js
@@ -3,7 +3,35 @@ function computerPlay(computerSelection) {
  computerSelection = rockPaperScssrs[Math.floor(Math.random() * rockPaperScssrs.length)]
  //computerSelection = Math.floor(Math.random() * rockPaperScssrs.length)
 console.log(computerSelection)
+return computerSelection
 }
 
+function gamePlay(playerSelection, computerSelection) {
+    computerSelection = computerPlay()
+    playerSelection = prompt()
 
-computerPlay()
+    if (playerSelection == computerSelection) {
+        alert("Tie!")
+    }
+
+    if(playerSelection == 'rock' && computerSelection == 'paper') {
+        alert("You lose!")
+    }
+    if(playerSelection == 'scizzors' && computerSelection == 'paper') {
+        alert("You win!")
+    }
+    if(playerSelection == 'paper' && computerSelection == 'rock') {
+        alert("You win!")
+    }
+    if(playerSelection == 'scizzors' && computerSelection == 'rock') {
+        alert("You lose!")
+    }
+    if(playerSelection == 'rock' && computerSelection == 'scizzors') {
+        alert("You win!")
+    }
+    if(playerSelection == 'paper' && computerSelection == 'scizzors') {
+        alert("You lose!")
+    }
+}
+
+gamePlay()
