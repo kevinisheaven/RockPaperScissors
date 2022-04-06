373e5c07ebf85dd25aa5e0eb8495cf67963ce179
Kevin Horwitz <khorwitz1992@gmail.com>
2022-04-05 21:20:49 -0500

    added random string function

---
+++ index.js
@@ -0,0 +1,9 @@
+function computerPlay(computerSelection) {
+ let rockPaperScssrs = ['rock', 'paper', 'scizzors']
+ computerSelection = rockPaperScssrs[Math.floor(Math.random() * rockPaperScssrs.length)]
+ //computerSelection = Math.floor(Math.random() * rockPaperScssrs.length)
+console.log(computerSelection)
+}
+
+
+computerPlay()
