2836188b6ab3f96315b7e4a0d71f0d7664ad38a4
Kevin Horwitz <khorwitz1992@gmail.com>
2022-04-09 12:41:39 -0500

    refined results string interpolation

--- index.js
+++ index.js
@@ -9,29 +9,52 @@ return computerSelection
 function gamePlay(playerSelection, computerSelection) {
     computerSelection = computerPlay()
     playerSelection = prompt()
+    playerSelection = playerSelection.toLowerCase()
+    result = ``
 
     if (playerSelection == computerSelection) {
         alert("Tie!")
+        result = "Tie!"
+        console.log(result)
+        return result
     }
-
     if(playerSelection == 'rock' && computerSelection == 'paper') {
         alert("You lose!")
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        console.log(result)
+        return result
     }
     if(playerSelection == 'scizzors' && computerSelection == 'paper') {
         alert("You win!")
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+        console.log(result)
+        return result
     }
     if(playerSelection == 'paper' && computerSelection == 'rock') {
         alert("You win!")
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+        console.log(result)
+        return result
     }
     if(playerSelection == 'scizzors' && computerSelection == 'rock') {
         alert("You lose!")
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        console.log(result)
+        return result
     }
     if(playerSelection == 'rock' && computerSelection == 'scizzors') {
         alert("You win!")
+        result = `You win! ${playerSelection} beats ${computerSelection}`
+        console.log(result)
+        return result
     }
     if(playerSelection == 'paper' && computerSelection == 'scizzors') {
         alert("You lose!")
+        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        console.log(result)
+        return result
     }
+    //console.log(result)
 }
 
 gamePlay()
