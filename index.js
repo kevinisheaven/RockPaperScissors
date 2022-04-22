c58c9c34271f077b9fbf145767d6f743dad9c170
Kevin Horwitz <khorwitz1992@gmail.com>
2022-04-22 17:38:52 -0500

    finalized scoring

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ let playerSelection;
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay()
-    //playerSelection = playerSelection;
-   // playerSelection = prompt()
-    //playerSelection = playerSelection.toLowerCase()
-    //result = ``
     let result = ``;
     let rpsResult = document.querySelector('#result')
     let par = document.createElement('p')
@@ -23,77 +19,55 @@ function playRound(playerSelection, computerSelection) {
     let cScore = document.querySelector('.computer')
 
     if (playerSelection == computerSelection) {
-        alert("Tie!")
         par.textContent = "Tie!"
-        console.log(result)
-        //return result
         rpsResult.appendChild(par)
     }
     if(playerSelection == 'rock' && computerSelection == 'paper') {
-        alert("You lose!")
         par.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
-        //console.log(result)
         compScore++
         num.textContent = compScore
         console.log(compScore)
-        //return result
         rpsResult.appendChild(par)
         cScore.appendChild(num)
         
     }
     if(playerSelection == 'scizzors' && computerSelection == 'paper') {
-        alert("You win!")
         par.textContent = `You win! ${playerSelection} beats ${computerSelection}`
-        console.log(result)
         playerScore++
         num.textContent = playerScore
         console.log(playerScore)
         rpsResult.appendChild(par)
-        //return result
         pScore.appendChild(num)
 
     }
     if(playerSelection == 'paper' && computerSelection == 'rock') {
-        alert("You win!")
         par.textContent = `You win! ${playerSelection} beats ${computerSelection}`
-        console.log(result)
         playerScore++
         num.textContent = playerScore
         console.log(playerScore)
         rpsResult.appendChild(par)
         pScore.appendChild(num)
 
-      //  return result
     }
     if(playerSelection == 'scizzors' && computerSelection == 'rock') {
-        alert("You lose!")
         par.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
-        console.log(result)
         compScore++
         num.textContent = compScore
         console.log(compScore)
-       // return result
        rpsResult.appendChild(par)
        cScore.appendChild(num)
 
     }
     if(playerSelection == 'rock' && computerSelection == 'scizzors') {
-        alert("You win!")
         par.textContent = `You win! ${playerSelection} beats ${computerSelection}`
-        //console.log(result)
         playerScore++
         num.textContent = playerScore
         console.log(playerScore)
         rpsResult.appendChild(par)
         pScore.appendChild(num)
 
-        //return result
-        //console.log(result)
-        //rpsResult.appendChild(result)
-
     }
     if(playerSelection == 'paper' && computerSelection == 'scizzors') {
-        alert("You lose!")
         par.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
         console.log(result)
         compScore++
@@ -103,18 +77,17 @@ function playRound(playerSelection, computerSelection) {
         cScore.appendChild(num)
         //return result
     }
+    if(playerScore == 5) {
+        alert("Player wins, down with the machines!!")
+        location.reload()
+    }
+    if (compScore == 5){
+        alert("Computer wins, you suck!")
+        location.reload()
+    }
 }
 
 function game() {
-    // for (let i = 0; i < 5; i++) {
-    //     playRound()
-    // }
-    // if (playerScore > compScore) {
-    //     alert("Player wins!!")
-    // } else {
-    //     alert("Computer wins, you suck!")
-    // }
-
 const buttons = document.querySelectorAll('button');
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
@@ -128,10 +101,7 @@ buttons.forEach((button) => {
     }
   });
 });
-    // let rock = document.querySelector('rock')
-    // let paper = document.querySelector('paper')
-    // let scizzors = document.querySelector('scizzors')
-
+    
 }
 
 game()
